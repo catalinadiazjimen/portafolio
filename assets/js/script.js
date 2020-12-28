5c634e7c0fc1e88b4688780bb9a3bc66e1cdae2e
Catalina <catalina.diazjimen@gmail.com>
2020-12-27 23:52:13 -0300

    portafolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,11 @@
 // JavaScript Document
-$(function () 
-	
-// typed
-
 // toggle menu nav-mobile
-$('body').on('click', '.mobile-nav-toggle', function() {
+$(document).on('click', '.mobile-nav-toggle', function() {
     $('body').toggleClass('mobile-nav-active');
     $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
 });
 
-$("document").click(function(e) {
+$(document).click(function(e) {
     var container = $(".mobile-nav-toggle");
     if (!container.is(e.target) && container.has(e.target).length === 0) {
       if ($('body').hasClass('mobile-nav-active')) {
@@ -41,65 +37,12 @@ var nav_sections = $('section');
     });
  });
 
-
-// Isotope para filtro portafolio
-$(window).on('load', function() {
-	var portfolioIsotope = $('.portfolio-container').isotope({
-	itemSelector: '.portfolio-item'
-});
-
-$('#portfolio-flters li').on('click', function() {
-$("#portfolio-flters li").removeClass('filter-active');
-$(this).addClass('filter-active');
-
-portfolioIsotope.isotope({
-filter: $(this).data('filter')
-});
-aos_init();
-});
-// Venobox (caja de luz utilizada en portafolio)
 $('.venobox').venobox({
 'share': false,
 framewidth: 'default', 
 frameheight: 'default',
 titleattr: 'false'
 }); 
-	
-// OwlCarousel (Portafolio detalle) //
-$(".portfolio-details-carousel").owlCarousel({
-	autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-});
-AOS.init({
-	offset: 120,
-	duration: 400,
-	easing: 'ease'
-});
-	
-// OwlCarousel (Brand Design) //
-$('.owl-carousel').owlCarousel({
-    center: true,
-    items:2,
-    loop:true,
-    margin:10,
-    responsive:{
-    0:{
-      items:3
-    },
-    600:{
-      items:3
-    },
-	700:{
-	  items: 4
-	},
-    1000:{
-      items:5
-    }
-    }
-});
-	
 
 
 });
